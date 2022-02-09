93cd5711ca989980569b551e6d63730f05902642
jcamilofarfan <farfan1976@hotmail.com>
2022-02-09 11:28:47 -0500

    Tipos de datos abstractos y clases, Instancias

---
+++ persona.py
@@ -0,0 +1,17 @@
+#Definicion
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saluda(self, otra_persona):
+        return f'Hola {otra_persona.nombre}, me llamo {self.nombre}.'
+
+#Uso
+david = Persona('David', 35)
+erika = Persona('Erika', 12)
+
+david.saluda(erika)
+print(david.saluda(erika))
+# 'Hola Erika, me llamo David.'
+
